f17e8d363af40c840f5d38714f197fba06aaae87
parimisetti lakshman <parimisetti lakshman@LAPTOP-UUF6J9V9>
2020-05-05 16:24:02 +0530

    Added zuulproxy Configuration

--- ServiceServer/src/main/java/com/example/ServiceServer/ServiceServerApplication.java
+++ ServiceServer/src/main/java/com/example/ServiceServer/ServiceServerApplication.java
@@ -1,10 +1,13 @@
 package com.example.ServiceServer;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaServer
 @SpringBootApplication
+@EnableZuulProxy
 public class ServiceServerApplication {
 
 	public static void main(String[] args) {
